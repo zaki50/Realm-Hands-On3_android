32f85cb7869670afa431d52ea9accf6d34cc14a4
Makoto Yamazaki <makoto1975@gmail.com>
2016-06-11 03:16:33 +0900

    「3.タイムライン表示の作成」完了状態

---
+++ Twitter/app/src/main/java/io/realm/handson3/twitter/TimelineFragment.java
@@ -0,0 +1,44 @@
+package io.realm.handson3.twitter;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.Arrays;
+
+public class TimelineFragment extends ListFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final ListAdapter adapter = new ArrayAdapter<String>(getContext(),
+                R.layout.listitem_tweet,
+                R.id.text,
+                Arrays.asList("Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+                ));
+
+        setListAdapter(adapter);
+    }
+}
